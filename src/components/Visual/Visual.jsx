109f7a9c22300fd92ba2f161c1510816926e9131
Alex Tiutiunyk <alex.tiutiunyk@gmail.com>
2025-01-07 19:23:22 +0200

    Update visual box

--- src/components/Visual/Visual.jsx
+++ src/components/Visual/Visual.jsx
@@ -3,7 +3,6 @@ import './visual.scss';
 const Visual = () => {
   return (
     <div className='visual-box'>
-      <img src='/images/visual.jpg' alt='P2P Cryptocurrency Exchange Platform' />
       <h1 className='visual-box__title'>
         P2P
         <br />
