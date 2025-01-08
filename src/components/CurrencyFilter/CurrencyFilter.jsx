6bdafcaa313b94dd5ae73730e2710d73ed51606f
Alex Tiutiunyk <alex.tiutiunyk@gmail.com>
2025-01-08 21:34:24 +0200

    Add content in Buy BTC tab

--- src/components/CurrencyFilter/CurrencyFilter.jsx
+++ src/components/CurrencyFilter/CurrencyFilter.jsx
@@ -13,7 +13,36 @@ const CurrencyFilter = () => {
         </TabList>
 
         <TabPanel>
-          <h2>Any content 1</h2>
+          <div className='form-row inline'>
+            <label htmlFor='currency'>Currency to Use for Purchase</label>
+            <div className='form-row_holder'>
+              <select id='currency'>
+                <option selected>ILS</option>
+                <option>USD</option>
+              </select>
+            </div>
+          </div>
+          <div className='form-row inline'>
+            <label htmlFor='method'>Payment method</label>
+            <div className='form-row_holder'>
+              <select id='method'>
+                <option selected>Cash</option>
+                <option>Bank Transfer</option>
+              </select>
+              <div className='chip-container'>
+                <button className='badge'>Cash</button>
+                <button className='badge'>Bank Transfer</button>
+              </div>
+            </div>
+          </div>
+          <div className='form-row inline'>
+            <label htmlFor='purchase'>Purchase</label>
+            <div className='form-row_holder'>
+              <div className='text-field' value='USD'>
+                <input type='text' id='purchase' />
+              </div>
+            </div>
+          </div>
         </TabPanel>
         <TabPanel>
           <h2>Any content 2</h2>
--- src/components/WelcomeSection/WelcomeSection.jsx
+++ src/components/WelcomeSection/WelcomeSection.jsx
@@ -6,7 +6,7 @@ const WelcomeSection = () => {
     <section className='welcome-section'>
       <div className='welcome-section__container'>
         <header className='welcome-section__head'>
-          <h1 className='welcome-section_title'>Welcome to Hodl Hodl</h1>
+          <h1 className='welcome-section_title'>Welcome to Zojax</h1>
           <p>Anonymous P2P deals on your terms. Trade globally.</p>
           <p>
             Using any payment system. For any <a href='#'>currency</a>.
